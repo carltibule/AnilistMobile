a47b13f7a2a8527f0763d32f35b830efee73759f
Carl Tibule <ctibule@outlook.com>
2018-10-22 15:11:24 -0500

    Edited MainActivity to add Apollo client

--- app/src/main/java/ca/ctibule/anilistmobile/MainActivity.java
+++ app/src/main/java/ca/ctibule/anilistmobile/MainActivity.java
@@ -3,11 +3,21 @@ package ca.ctibule.anilistmobile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.apollographql.apollo.ApolloClient;
+
+import okhttp3.OkHttpClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String ANILIST_API_URL= "https://graphql.anilist.co";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        ApolloClient apolloClient = ApolloClient.builder().serverUrl(ANILIST_API_URL).okHttpClient(okHttpClient).build();
+
     }
 }

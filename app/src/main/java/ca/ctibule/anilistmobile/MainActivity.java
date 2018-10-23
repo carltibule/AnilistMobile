72e05a135a15d4814d6a333196a5af2131b08059
Carl Tibule <ctibule@outlook.com>
2018-10-22 19:31:44 -0500

    Modified project to properly support Apollo

--- app/src/main/java/ca/ctibule/anilistmobile/MainActivity.java
+++ app/src/main/java/ca/ctibule/anilistmobile/MainActivity.java
@@ -1,10 +1,19 @@
 package ca.ctibule.anilistmobile;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
 
+import org.jetbrains.annotations.NotNull;
+
+import ca.ctibule.AnilistMobile.MediaQuery;
+import ca.ctibule.AnilistMobile.type.MediaSeason;
 import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
         ApolloClient apolloClient = ApolloClient.builder().serverUrl(ANILIST_API_URL).okHttpClient(okHttpClient).build();
+
+        apolloClient.query(
+                MediaQuery.builder().page(1).season(MediaSeason.WINTER).year(2018).build()
+        ).enqueue(new ApolloCall.Callback<MediaQuery.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<MediaQuery.Data> response) {
+                Log.d("GraphQL", response.toString());
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+                Log.d("GraphQL", e.getMessage());
+            }
+        });
     }
 }

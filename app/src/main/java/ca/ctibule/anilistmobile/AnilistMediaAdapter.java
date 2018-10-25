931b5155022204f04b11b0168563ffe2137c71b9
Carl Tibule <ctibule@outlook.com>
2018-10-25 15:32:01 -0500

    Created new adapter for list of media pulled from API

---
+++ app/src/main/java/ca/ctibule/anilistmobile/AnilistMediaAdapter.java
@@ -0,0 +1,57 @@
+package ca.ctibule.anilistmobile;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnilistMediaAdapter extends ArrayAdapter<AnilistMedia> {
+    private ArrayList<AnilistMedia> anilistMedia = new ArrayList<AnilistMedia>();
+
+    public AnilistMediaAdapter(@NonNull Context context, int resource, @NonNull ArrayList<AnilistMedia> anilistMedia) {
+        super(context, resource, anilistMedia);
+        this.anilistMedia = anilistMedia;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = convertView;
+
+        if(v == null){
+            LayoutInflater vi = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.lyt_media, null);
+        }
+
+        AnilistMedia media = anilistMedia.get(position);
+
+        if(media != null){
+            ImageView imgCoverImage = v.findViewById(R.id.img_cover_image);
+            TextView lblTitle = v.findViewById(R.id.lbl_title);
+            TextView lblDescription = v.findViewById(R.id.lbl_description);
+
+            if(lblTitle != null){
+                if(media.getRomajiTitle() != null){
+                    lblTitle.setText(media.getRomajiTitle());
+                }
+                else{
+                    lblTitle.setText(media.getEnglishTitle());
+                }
+            }
+
+            if(lblDescription != null){
+                lblDescription.setText(media.getDescription());
+            }
+        }
+
+        return v;
+    }
+}

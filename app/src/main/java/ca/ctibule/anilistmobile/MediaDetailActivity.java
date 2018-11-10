b1a9586eb326d72b191a3b12638bfa56a629c881
Carl Tibule <ctibule@outlook.com>
2018-11-10 00:54:09 -0600

    Fixed TabLayout and ViewPager. Fragments now only show up in the lower-section of the activity. Also synced movements of the fragment with the TabLayout. Source: https://android.jlelse.eu/tablayout-and-viewpager-in-your-android-app-738b8840c38a

--- app/src/main/java/ca/ctibule/anilistmobile/MediaDetailActivity.java
+++ app/src/main/java/ca/ctibule/anilistmobile/MediaDetailActivity.java
@@ -1,6 +1,14 @@
 package ca.ctibule.anilistmobile;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,10 +35,17 @@ import ca.ctibule.anilistmobile.models.AnilistMedia;
 import ca.ctibule.anilistmobile.tasks.DownloadImageTask;
 import okhttp3.OkHttpClient;
 
-public class MediaDetailActivity extends AppCompatActivity {
+public class MediaDetailActivity extends AppCompatActivity implements MediaDetailSummaryFragment.OnFragmentInteractionListener,
+EpisodesFragment.OnFragmentInteractionListener, ExternalLinksFragment.OnFragmentInteractionListener{
 
     private static final String ANILIST_API_URL= "https://graphql.anilist.co";
     private AnilistMedia media;
+    private FragmentPagerAdapter adapterViewPager;
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +56,13 @@ public class MediaDetailActivity extends AppCompatActivity {
 
         QueryAPIByIdTask queryAPIByIdTask = new QueryAPIByIdTask();
         queryAPIByIdTask.execute();
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        adapterViewPager = new PagerAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(adapterViewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 
@@ -177,6 +199,50 @@ public class MediaDetailActivity extends AppCompatActivity {
                 }
 
                 new DownloadImageTask(imgDetailCoverImage).execute(coverImageLink);
+            }
+        }
+    }
+
+    public static class PagerAdapter extends FragmentPagerAdapter{
+        private static int NUM_ITEMS = 3;
+        private Context context;
+
+        public PagerAdapter(Context context, FragmentManager fragmentManager){
+            super(fragmentManager);
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch(i){
+                case 0:
+                    return new MediaDetailSummaryFragment();
+                case 1:
+                    return new EpisodesFragment();
+                case 2:
+                    return new ExternalLinksFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0:
+                    return context.getString(R.string.summary);
+                case 1:
+                    return context.getString(R.string.episodes);
+                case 2:
+                    return context.getString(R.string.links);
+                default:
+                    return null;
             }
         }
     }
